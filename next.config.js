8a3eec99eb75d17b182666570c480f86181ab3cd
ismayil4asim-code <ismayil4asim@gmail.com>
2026-04-02 18:08:02 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,34 +5,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
   images: {
     domains: ['*.supabase.co', 'i.ibb.co', 'imgbb.com'],
     unoptimized: true,
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 31536000,
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['framer-motion', 'react-icons'],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   poweredByHeader: false,
-  generateEtags: true,
-  httpAgentOptions: {
-    keepAlive: true,
-  },
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
-  },
-  staticPageGenerationTimeout: 120,
-  output: 'standalone',
-  trailingSlash: false,
-  productionBrowserSourceMaps: false,
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
